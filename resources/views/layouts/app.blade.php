cf86b80ac29c242b0a8985edf3d7046248b4537d
raflialviro84 <raflialviro84@gmail.com>
2025-07-25 23:44:38 +0700

    fix: update CSS link in Profil Desa to use home.css for consistency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Desa Kampung Anyar')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     @yield('css')
 </head>
--- resources/views/profil-desa.blade.php
+++ resources/views/profil-desa.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Profil Desa')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/profil-desa.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
 @endsection
