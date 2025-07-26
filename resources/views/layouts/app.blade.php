a9320ebb12032b64a8d3b907ec3f0a4b18ec5824
Nadiyah myrilla <143943300+nadiyahmyrilla@users.noreply.github.com>
2025-07-26 13:58:28 +0700

    Menambahkan file RT&RW

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('gambar-icon/logo-desa.png') }}">
     @yield('css')
 </head>
 <body class="bg-gray-50 font-sans">
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\PageController;
 
 Route::get('/', [PageController::class, 'beranda'])->name('beranda');
 Route::get('/profil-desa', [PageController::class, 'profilDesa'])->name('profil-desa');
+Route::get('/pemerintahan-RT-RW', function () {return view('pemerintahan.RT_RW');})->name('pemerintahan-RT-RW');
 Route::get('/kontak', [PageController::class, 'kontak'])->name('kontak');
