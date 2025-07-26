8f70359925f6d8642ae2db4249b688e111fd488b
raflialviro84 <raflialviro84@gmail.com>
2025-07-26 13:33:53 +0700

    feat: add berita and galeri pages with corresponding routes and styles

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ use App\Http\Controllers\PageController;
 Route::get('/', [PageController::class, 'beranda'])->name('beranda');
 Route::get('/profil-desa', [PageController::class, 'profilDesa'])->name('profil-desa');
 Route::get('/kontak', [PageController::class, 'kontak'])->name('kontak');
+Route::get('/berita', [PageController::class, 'berita'])->name('berita');
+Route::get('/galeri', [PageController::class, 'galeri'])->name('galeri');
